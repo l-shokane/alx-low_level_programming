174cbd171d0ef95d8de128e443e63f874de5d398
l-shokane <masedie.rev@gmail.com>
2023-10-23 17:01:14 -0700

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - main function
+ * @s: function parameter 1
+ * @accept: function parameter 2
+ * Return: always 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int _strspn(char *s, char *accept);
+	unsigned int i = 0;
+	int k;
+
+	while(*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+			{
+				i++;
+				break;
+			}
+			else if (accept[k + 1] == '\0')
+				return (i);
+		}
+		s++;
+	}
+	return (i);
+}
